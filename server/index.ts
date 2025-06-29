fb542c0f3514fcf333aa1fa3da7f7b793d2cdafd
skillbasket <shivanisharma232005@gmail.com>
2025-06-29 19:20:06 +0530

    hi

--- server/index.ts
+++ server/index.ts
@@ -7,7 +7,7 @@ import { setupAuth, isAuthenticated } from "./googleAuth"; //
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
-import { setupAuth, isAuthenticated } from "./";
+
 
 const app = express();
 app.use(express.json());
